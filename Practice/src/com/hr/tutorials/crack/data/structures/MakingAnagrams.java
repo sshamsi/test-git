5f6d6328308aeaf8a9f4d7a478f7a178c6e07238
shazebshamsi <shazebshamsi@192.168.1.158>
2017-03-19 21:12:12 -0400

    A more efficient solution

--- Practice/src/com/hr/tutorials/crack/data/structures/MakingAnagrams.java
+++ Practice/src/com/hr/tutorials/crack/data/structures/MakingAnagrams.java
@@ -1,12 +1,13 @@
 package com.hr.tutorials.crack.data.structures;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class MakingAnagrams {
 	
-    public static int numberNeeded(String first, String second) {
+    public static int numberNeededSolution1(String first, String second) {
         List<Character> firstList = new ArrayList<Character>();  
         for(int i=0 ; i<first.length() ; i++){
             firstList.add(first.charAt(i));
@@ -29,12 +30,19 @@ public class MakingAnagrams {
         return firstList.size() + secondList.size();  
       }
     
+    public static int numberNeededSolution2(String first, String second) {
+    	int[] freq = new int[26];
+    	first.chars().forEach(c -> freq[c - 'a']++);
+    	second.chars().forEach(c -> freq[c - 'a']--);
+    	return Arrays.stream(freq).map(Math::abs).sum();
+      }    
+    
       public static void main(String[] args) {
           Scanner in = new Scanner(System.in);
           String a = in.next();
           String b = in.next();
           in.close();
-          System.out.println(numberNeeded(a, b));
+          System.out.println(numberNeededSolution2(a, b));
       }
 
 }
