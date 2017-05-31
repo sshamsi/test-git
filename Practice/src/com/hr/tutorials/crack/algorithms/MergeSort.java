047f0af4532a33d7ff094f02589744b250b0cbe9
sshamsi <sshamsi@daon100963.daon.com>
2017-05-31 16:38:52 -0400

    Merge sort completed

--- Practice/src/com/hr/tutorials/crack/algorithms/MergeSort.java
+++ Practice/src/com/hr/tutorials/crack/algorithms/MergeSort.java
@@ -5,39 +5,28 @@ import java.util.Arrays;
 public class MergeSort {
 
 	public static void main(String[] args) {
-		//int num[] = {5,4,3,2,1,10,9,8,7,6};
 		int num[] = {4,3,2,1};
-		mergeSort(num, 0, num.length-1);
-		System.out.println(Arrays.toString(num));
+		int[] mergedArray = mergeSort(num);
+		System.out.println(Arrays.toString(mergedArray));
 
 	}
 	
-	public static void mergeSort(int[] arr, int left, int right){
-		
-		if(left<right){
-			int middle = (left + right)/2;
-			mergeSort(arr, left, middle);
-			mergeSort(arr, middle+1, right);
-			merge(arr, left, middle, right);
+	public static int[] mergeSort(int[] array){
+		if(array.length==1){
+			return array;
 		}
-		
-	}
-	
-	public static void merge(int[] arr, int left, int middle, int right){
-		int[] arr1 = new int[middle - left + 1];
-		for(int i=0; i< arr1.length ; i++){
-			arr1[i] = arr[left + i];
-		}
-		int[] arr2 = new int[right-middle];
-		for(int j=0; j < arr2.length ; j++){
-			arr2[j] = arr[left + middle + j];
-		}
-		merge(arr1, arr2);
+		int[] leftHalf = Arrays.copyOfRange(array, 0, (array.length/2));
+		int[] rightHalf = Arrays.copyOfRange(array, array.length/2, array.length);
+
+		leftHalf = mergeSort(leftHalf);
+		rightHalf = mergeSort(rightHalf);
+		System.out.println(Arrays.toString(leftHalf));
+		System.out.println(Arrays.toString(rightHalf));
+		System.out.println("Calling merge ");
+		return merge(leftHalf, rightHalf);
 	}
 	
 	public static int[] merge(int[] arr1, int[] arr2){
-		System.out.println("merge(int[],int[])"+Arrays.toString(arr1));
-		System.out.println("merge(int[],int[])"+Arrays.toString(arr2));
 		int[] mergedArray = new int[arr1.length+arr2.length];
 		
 		int i=0,j=0,k=0;
@@ -58,7 +47,6 @@ public class MergeSort {
 		while(j<arr2.length){
 			mergedArray[k++] = arr2[j++];
 		}
-		
 		return mergedArray;
 		
 	}
