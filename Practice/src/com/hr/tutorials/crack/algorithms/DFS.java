c28e153017dc52baba63ac3641ea562351c40b62
sshamsi <sshamsi@daon100963.daon.com>
2017-07-20 17:57:26 -0400

    dfs iterative

--- Practice/src/com/hr/tutorials/crack/algorithms/DFS.java
+++ Practice/src/com/hr/tutorials/crack/algorithms/DFS.java
@@ -5,15 +5,20 @@ import java.util.List;
 import java.util.ListIterator;
 
 /*
- * Inorder (Left, Root, Right) : 
+ * Inorder (Left, Root, Right) : Default Depth First Search
  * Preorder (Root, Left, Right) : List of vertices in the order they were first visited by DFS algorithm
  * Postorder (Left, Right, Root) : List of vertices in the order they were last visited by DFS algorithm
  */
 public class DFS {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Graph g = new Graph(5);
+		g.addEdge(1, 2);
+		g.addEdge(1, 3);
+		g.addEdge(2, 4);
+		g.addEdge(2, 5);
 
+		g.preOrderDFSIterative(1);
 	}
 	
 	public static class Graph{
@@ -22,9 +27,9 @@ public class DFS {
 		
 		@SuppressWarnings("unchecked")
 		public Graph(int v){
-			vertices = v;
-			adjacencyList = new LinkedList[v];
-			for(int i=0 ; i<v ; i++){
+			vertices = v+1;
+			adjacencyList = new LinkedList[vertices];
+			for(int i=0 ; i<vertices ; i++){
 				adjacencyList[i] = new LinkedList<Integer>();
 			}
 		}
@@ -33,14 +38,20 @@ public class DFS {
 			adjacencyList[u].add(v);
 		}
 		
-		public void preOrderDFS(int s){
+		public void preOrderDFSIterative(int s){
+			boolean[] visited = new boolean[vertices];
 			LinkedList<Integer> list = new LinkedList<Integer>();
 			list.push(s);
 			while(!list.isEmpty()){
 				int element = list.pop();
 				System.out.print(element+" ");
+				visited[element] = true;
 				ListIterator<Integer> iterator = adjacencyList[element].listIterator();
 				while(iterator.hasNext()){
+					int node = iterator.next();
+					if(!visited[node]){
+						list.push(node);
+					}
 				}
 			}
 		}
