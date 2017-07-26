2a7ccf498f04afc4ce13987d3bf80f1bd2ba66f6
sshamsi <sshamsi@daon100963.daon.com>
2017-07-26 17:38:32 -0400

    Super reduced string

---
+++ Practice/src/com/hr/algorithms/strings/SuperReducedString.java
@@ -0,0 +1,89 @@
+package com.hr.algorithms.strings;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 
+ * @author sshamsi
+ * https://www.hackerrank.com/challenges/reduced-string/problem
+ * Problem: Delete any pair of adjacent letters with same value.
+ * Repeat the above operation as many times as it can be performed. 
+ */
+public class SuperReducedString {
+
+	/**
+	 * aaabccddd should become abd
+	 * aa should become empty string
+	 * baab should become empty string
+	 * brute force solution
+	 * @param s
+	 * @return reduced string
+	 */
+	 static String super_reduced_string(String s){
+		   
+	        StringBuilder sb = new StringBuilder(s);
+	        
+	        int i=0;
+	        while(i<sb.length()-1){
+	            if(sb.charAt(i)==sb.charAt(i+1)){
+	                sb.deleteCharAt(i);
+	                sb.deleteCharAt(i);
+	                i = 0;
+	            }
+	            else{
+	            	i++;
+	            }
+	        }
+	        
+	        if(sb.length()==0){
+	        	return "Empty String";
+	        }
+	        else{
+	        	return sb.toString();
+	        }
+	        
+	    }
+	 
+		/**
+		 * aaabccddd should become abd
+		 * aa should become empty string
+		 * baab should become empty string
+		 * efficient stack solution
+		 * @param s
+		 * @return reduced string
+		 */
+		 static String super_reduced_string_stack(String s){
+			   
+			   Deque<Character> stack = new ArrayDeque<Character>();
+			   
+			   for(int i=0; i<s.length() ;i++){
+				   if(!stack.isEmpty() && stack.peek()==s.charAt(i)){
+					   stack.pop();
+				   }
+				   else{
+					   stack.push(s.charAt(i));
+				   }
+			   }
+			   
+			   StringBuilder sb = new StringBuilder();
+			   while(!stack.isEmpty()){
+				   sb.append(stack.pop());
+			   }
+			 
+			   if(sb.length()==0){
+		        	return "Empty String";
+		        }
+		        else{
+		        	return sb.reverse().toString();
+		        }
+		        
+		    }
+
+	    public static void main(String[] args) {
+	        String s = "aaabccddd";
+	        String result = super_reduced_string_stack(s);
+	        System.out.println(result);
+	    }
+
+}
