c46e135b05ab586c8e0e99d022c0da882742253e
sshamsi <sshamsi@daon100963.daon.com>
2017-10-13 18:40:43 -0400

    Valid String

--- Practice/src/com/hr/algorithms/strings/Sherlock.java
+++ Practice/src/com/hr/algorithms/strings/Sherlock.java
@@ -11,66 +11,81 @@ public class Sherlock {
 	
 	@Test
 	public void test1(){
-		Assert.assertEquals("NO", isValid1("aabbcd"));
+		Assert.assertEquals("NO", isValid2("aabbcd"));
 	}
 	
 	@Test
 	public void test2(){
-		Assert.assertEquals("YES", isValid1("abbcc"));
+		Assert.assertEquals("YES", isValid2("abbcc"));
 	}
 	
 	@Test
 	public void test3(){
-		Assert.assertEquals("YES", isValid1("aabbcc"));
+		Assert.assertEquals("YES", isValid2("aabbcc"));
 	}
 	
 	@Test
 	public void test4(){
-		Assert.assertEquals("NO", isValid1("baacdd"));
+		Assert.assertEquals("NO", isValid2("baacdd"));
 	}
 	
 	@Test
 	public void test5(){
-		Assert.assertEquals("YES", isValid1("aabbccc"));
+		Assert.assertEquals("YES", isValid2("aabbccc"));
+	}
+	
+	@Test
+	public void test6(){
+		Assert.assertEquals("NO", isValid2("aaabbc"));
+	}
+	
+	@Test
+	public void test7(){
+		Assert.assertEquals("YES", isValid2("aaabbbc"));
 	}
 	
     static String isValid(String s){
         // Complete this function
-    	int[] c = new int[26];
-    	for(int i=0 ; i<s.length(); i++){
-    		c[s.charAt(i) - 97]++ ;
-    	}
-    	System.out.println(Arrays.toString(c));
-    	
-    	int max=0;
-    	for(int i=0 ; i<c.length; i++){
-    		max = Math.max(max, c[i]);
-    	}
-    	
-    	System.out.println(max);
-    	
-    	for(int i=0 ; i<c.length; i++){
-    		if(c[i]==max){
-    			c[i] = 0;
-    		}
-    	}
-    	
-    	System.out.println(Arrays.toString(c));
-    	
-    	int countNonZero=0;
-    	for(int i=0 ; i<c.length; i++){
-    		if(c[i]!=0){
-    			countNonZero++;
-    		}
-    	}
-    	
-    	if(countNonZero>1){
-    		return "NO";
-    	}
-    	else{
-    		return "YES";
-    	}
-    	
+      	int[] c = new int[26];
+      	for(int i=0 ; i<s.length(); i++){
+      		c[s.charAt(i) - 97]++ ;
+      	}
+      	
+      	int uniqueCharacters=0;
+      	for(int i=0 ; i<c.length; i++){
+      		if(c[i]!=0){
+      			uniqueCharacters++;
+      		}
+      	}
+      	
+      	int[] characterFrequency = new int[uniqueCharacters];
+      	int j=0;
+      	for(int i=0 ; i<c.length; i++){
+      		if(c[i]!=0){
+      			characterFrequency[j++] = c[i];
+      		}
+      	}
+      	
+      	Arrays.sort(characterFrequency);
+      	System.out.println(Arrays.toString(characterFrequency));
+      	
+      	int k = characterFrequency[characterFrequency.length-1];
+      	System.out.println("k is "+k);
+      	int count = 0;
+     	for(int i=characterFrequency.length-2 ; i>=0; i--){
+     		System.out.println("characterFrequency[i] is "+characterFrequency[i]);
+      		if(characterFrequency[i]!=k){
+      			count++;
+      		}
+      	}
+     	
+     	if(count>1){
+     		return "NO";
+     	}
+     	else{
+     		return "YES";
+     	}
+      	
     }
     
     static String isValid1(String s){
@@ -126,9 +141,74 @@ public class Sherlock {
     	else{
     		return "NO";
     	}
+    }
+    
+    static String isValid2(String s){
+        // Complete this function
+    	int[] c = new int[26];
+    	for(int i=0 ; i<s.length(); i++){
+    		c[s.charAt(i) - 97]++ ;
+    	}
+    	//System.out.println(Arrays.toString(c));
+    	
+    	int uniqueCharacters=0;
+    	for(int i=0 ; i<c.length; i++){
+    		if(c[i]!=0){
+    			uniqueCharacters++;
+    		}
+    	}
+    	
+    	int[] characterFrequency = new int[uniqueCharacters];
+    	int j=0;
+    	for(int i=0 ; i<c.length; i++){
+    		if(c[i]!=0){
+    			characterFrequency[j++] = c[i];
+    		}
+    	}
+    	System.out.println(Arrays.toString(characterFrequency));
+    	
+    	if(isFrequencyConstant(characterFrequency)){
+    		return "YES";
+    	}
+    	
+    	for(int i=0; i<characterFrequency.length ; i++){
+    		int k = characterFrequency[i];
+    		if(k==1){
+    			k++;
+    		}
+    		else{
+    			k--;	
+    		}
+    		j=0;
+    		while(j<characterFrequency.length){
+    			if(i==j){
+    				j++;
+    			}
+    			else if(characterFrequency[j]!=k){
+    				break;
+    			}
+    			else{
+    				j++;
+    			}
+    			
+    		}
+    		if(j==characterFrequency.length){
+    			return "YES";
+    		}
+    	}
     	
     	
-    	
+        return "NO";
+    }
+    
+    public static boolean isFrequencyConstant(int[] characterFrequency){
+    	int frequency = characterFrequency[0];
+    	for(int i=0 ; i<characterFrequency.length ;i++){
+    		if(frequency!=characterFrequency[i]){
+    			return false;
+    		}
+    	}
+    	return true;
     }
 
 }
