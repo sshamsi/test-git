0d3728922e3befee62c7b3f96be7a4376004f930
shazebshamsi <shazebshamsi@192.168.1.158>
2017-10-12 20:21:23 -0400

    Valid String

--- Practice/src/com/hr/algorithms/strings/Sherlock.java
+++ Practice/src/com/hr/algorithms/strings/Sherlock.java
@@ -11,27 +11,27 @@ public class Sherlock {
 	
 	@Test
 	public void test1(){
-		Assert.assertEquals("NO", isValid("aabbcd"));
+		Assert.assertEquals("NO", isValid1("aabbcd"));
 	}
 	
 	@Test
 	public void test2(){
-		Assert.assertEquals("YES", isValid("abbcc"));
+		Assert.assertEquals("YES", isValid1("abbcc"));
 	}
 	
 	@Test
 	public void test3(){
-		Assert.assertEquals("YES", isValid("aabbcc"));
+		Assert.assertEquals("YES", isValid1("aabbcc"));
 	}
 	
 	@Test
 	public void test4(){
-		Assert.assertEquals("NO", isValid("baacdd"));
+		Assert.assertEquals("NO", isValid1("baacdd"));
 	}
 	
 	@Test
 	public void test5(){
-		Assert.assertEquals("YES", isValid("aabbccc"));
+		Assert.assertEquals("YES", isValid1("aabbccc"));
 	}
 	
     static String isValid(String s){
@@ -70,6 +70,64 @@ public class Sherlock {
     	else{
     		return "YES";
     	}
+    	
+    }
+    
+    static String isValid1(String s){
+        // Complete this function
+    	int[] c = new int[26];
+    	for(int i=0 ; i<s.length(); i++){
+    		c[s.charAt(i) - 97]++ ;
+    	}
+    	System.out.println(Arrays.toString(c));
+    	
+        int firstNonZero=0;
+        boolean first=false;
+        int secondNonZero=0;
+        
+    	for(int i=0 ; i<s.length(); i++){
+    		if(c[i]!=0){
+    			if(!first){
+    				firstNonZero = c[i];
+    				first = true;
+    			}
+    			else if (c[i]!=firstNonZero){
+    				secondNonZero = c[i];
+    				break;
+    			}
+    		}
+    	}
+    	
+    	System.out.println("firstNonZero "+firstNonZero);
+    	System.out.println("secondNonZero "+secondNonZero);
+    	
+    	if(firstNonZero==0 || secondNonZero==0){
+    		return "YES";
+    	}
+    	
+    	int firstNonZeroCount=0;
+    	int secondNonZeroCount=0;
+    	
+    	for(int i=0 ; i<s.length(); i++){
+			if(c[i]==firstNonZero){
+				firstNonZeroCount++;
+			}
+			else if (c[i]==secondNonZero){
+				secondNonZeroCount++;
+			}
+    	}
+    	
+    	System.out.println("firstNonZeroCount "+firstNonZeroCount);
+    	System.out.println("secondNonZeroCount "+secondNonZeroCount);
+    	
+    	if(firstNonZeroCount-secondNonZeroCount==1 || secondNonZeroCount-firstNonZeroCount==1){
+    		return "YES";
+    	}
+    	else{
+    		return "NO";
+    	}
+    	
+    	
     	
     }
 
